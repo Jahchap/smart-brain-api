87dd937f22350d1f45325b2abbc10b2e55e65f36
WCI Abu Dhabi <nc.jahchap@gmail.com>
2019-12-26 11:36:59 +0400

    Added the correct database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = require('knex')({
 	client: 'pg',
 	connection: {
-		host: '127.0.0.1',
+		host: 'postgresql-fluffy-89739',
 		user: 'postgres',
 		password: '12345678',
 		database: 'smart-brain'
