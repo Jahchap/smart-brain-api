eff2097976d23adb702d62f07fcc077ea6023c9a
WCI Abu Dhabi <nc.jahchap@gmail.com>
2020-01-06 01:15:54 +0400

    Modified server.js for env file

--- server.js
+++ server.js
@@ -12,19 +12,21 @@ const image = require('./controllers/image');
 
 const db = require('knex')({
 	client: 'pg',
-	connection: {
+	//using ternary operator to check if in production or dev environment
+	//This is for the knex connection
+	connection: process.env.NODE_ENV !== 'production'? {
 		connectionString: process.env.DATABASE_URL,
 		ssl: true
-	} || {
-		host: DB_HOST,
-		user: DB_USER,
-		password: DB_PASS,
-		database: DB
+	} : {
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		database: process.env.DB
 	}
 });
 
 /*
-// This is same as the above
+// This is same as the above (where Knex is imported and called at the same time )
 const knex = require('knex');
 const db = knex({
 	client: 'pg',
@@ -49,15 +51,15 @@ app.use(cors());
 
 // Home route
 app.get('/', (req, res) => {
-	// res.json('Welcome. Please visit the front end at nc-smart-brain.herokuapp.com to access this app')
-	db.select('*')
-		.from('users')
-		.then(allUsers => {
-			res.json(allUsers);
-		})
-		.catch(err => {
-			res.status(404).json('Unable to Get all Users');
-		})
+	res.json('Welcome. Please visit the front end at nc-smart-brain.herokuapp.com to access this app')
+	// db.select('*')
+	// 	.from('users')
+	// 	.then(allUsers => {
+	// 		res.json(allUsers);
+	// 	})
+	// 	.catch(err => {
+	// 		res.status(404).json('Unable to Get all Users');
+	// 	})
 });
 
 // Register endpoint (route)) '/register'
@@ -74,6 +76,6 @@ app.post('/imageurl', (req, res) => { image.handleImageApiCall(req, res)});
 app.get('*', (req, res) => {res.status(404).send('That page doesn\'t exist!')});
 
 // Set up the port
-app.listen(process.env.PORT || 4000, () => {
+app.listen(process.env.NODE_ENV === 'production'? process.env.PORT : 4000, () => {
 	console.log(`App started on port ${process.env.PORT}`);
 });
